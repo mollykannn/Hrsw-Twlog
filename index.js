f6b929e52fc1b7c7e9a1500c10540c1689ffac87
Molly Kan <mollykannn@gmail.com>
2023-04-07 21:19:22 +0800

    last

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const client = require('https');
 const stdio = require('stdio');
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc');
@@ -12,18 +13,38 @@ const cmd = stdio.getopt({
   'to': {key: 'to', description: 'to', args: 1},
   'user': {key: 'to', description: 'user', args: 1}
 });
-const ouptputCSV = `./src/${cmd.from}.md`;
+const file = `./src/${cmd.from}.md`;
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
+if (!fs.existsSync('./src/images')) {
+  fs.mkdirSync('./src/images');
+}
 
-fs.writeFile(ouptputCSV, "", (err) => {
+fs.writeFile(file, "", (err) => {
   if (err) throw err;
 });
 
 const formatDate = (date) => {
-  return dayjs(date.replace(' · ',' ')).tz('Asia/Tokyo').format('D MMM, YYYY hh:mm:ss A');
+  return dayjs(date.replace(' · ',' ')).tz('Asia/Tokyo').format('D MMM, YYYY hh:mm A');
+}
+
+function downloadImage(url, filepath) {
+  return new Promise((resolve, reject) => {
+      client.get(url, (res) => {
+          if (res.statusCode === 200) {
+              res.pipe(fs.createWriteStream(filepath))
+                  .on('error', reject)
+                  .once('close', () => resolve(filepath));
+          } else {
+              // Consume response data to free up memory
+              res.resume();
+              reject(new Error(`Request Failed With a Status Code: ${res.statusCode}`));
+
+          }
+      });
+  });
 }
 
 (async () => {
@@ -31,31 +52,36 @@ const formatDate = (date) => {
   const page = await browser.newPage();
   await page.goto(`https://nitter.it/${cmd.user}/search?f=tweets&q=&since=${cmd.from}&until=${cmd.to}`);
   await page.exposeFunction("formatDate", formatDate);
+  await page.exposeFunction("downloadImage", downloadImage);
 
   while (true) {
-    const result = await page.evaluate(async () => {
+    const result = await page.evaluate(async (fromDate) => {
       const elements = document.querySelectorAll(".timeline-item:not(.show-more)");
       let data = '';
-      for (let element of elements) {
+      for (let [index, element] of elements.entries()) {
         const isRetweet = element.querySelector(".retweet-header");
         const link = element.querySelector(".tweet-link");
         const date = element.querySelector(".tweet-date a")?.getAttribute("title");
         const content = element.querySelector(".tweet-content")?.innerHTML;
         const image = element.querySelector(".attachment img")?.getAttribute("src");
-        data += `[${await formatDate(date.toString())}](${link.toString().replaceAll('https://nitter.it/','https://twitter.com/')})  \n`;
+        let dateFormat = await formatDate(date.toString());
+        data += `[${dateFormat}](${link.toString().replaceAll('https://nitter.it/','https://twitter.com/')})  \n`;
         if (isRetweet) {
           const fullName = element.querySelector(".fullname-and-username .fullname").innerText;
           const username = element.querySelector(".fullname-and-username .username");
-          data += `Retweeted [${fullName} ${username.innerText}](${username})  \n\n`;
+          data += `Retweeted [${fullName} ${username.innerText}](https://twitter.com${username?.getAttribute("href")})  \n\n`;
         }
         data += `${content}  \n`;
-        if (image) data += `![image](https://nitter.it${image})  \n`;
+        if (image) {
+          await downloadImage(`https://nitter.it${image}`, `src/images/${fromDate}-${index}.png`);
+          data += `![image](images/${fromDate}-${index}.png)  \n`;
+        }
         data += `\n---\n\n`;
       }
       return data;
-    });
+    }, cmd.from);
 
-    fs.appendFile(ouptputCSV, result, (err) => {
+    fs.appendFile(file, result, (err) => {
       if (err) throw err;
     });
 
